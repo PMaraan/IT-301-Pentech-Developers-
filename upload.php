10e9ead3bac3409a7809a206b9240063397af641
PMaraan <maraanpatrick@gmail.com>
2024-11-25 11:01:09 +0800

    Delete upload.php

--- upload.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-    <form action="verifyupload.php" method="post" enctype="multipart/form-data">
-      <label>Select image to upload</label><br>
-      <input type="file" name="imgUpload"><br>
-      <input type="submit" name="submitBtn" value="Upload">
-    </form>
-  </body>
-</html>
